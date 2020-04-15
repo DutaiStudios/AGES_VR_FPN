6d101359cd1a836e8abda61688e28e7ede9b52a1
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-04-15 12:28:20 -0400

    Milestone 3 Code Review
    3 Interactive objects
    2 Color flashlight system
    Placeholder FPS camera system

---
+++ Assets/Scripts/RedlightControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedlightControl : MonoBehaviour
+{
+    [SerializeField] GameObject trueself;
+    private bool cansee = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Redlight")
+        {
+            cansee = true;
+        }
+
+        if (cansee == true)
+        {
+            trueself.SetActive(true);
+        }
+
+        else if (other.tag != "Redlight")
+        {
+            trueself.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Redlight")
+        {
+            trueself.SetActive(false);
+        }
+    }
+
+}
